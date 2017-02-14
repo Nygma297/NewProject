1f087957acb0b5d847dc131c136799f7065d33dc
Aniket Munshi <Aniket Munshi>
2017-02-14 12:17:29 +0530

    Added Routes

--- React_end/src/index.js
+++ React_end/src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { App, Home, Register, Login } from './App';
 import './index.css';
-import '../semantic/dist/semantic.min.css';
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Register} />
+      <Route path="Event" component={Event} />
+      <Route path="Login" component={Login} />
+      <Route path="Register" component={Register} />
+    </Route>
+  </Router>
+
+), document.getElementById('root'));
